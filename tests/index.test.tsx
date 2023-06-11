592e9ac3f46923e01d9290c6f3b326a8f279f227
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2023-06-11 18:52:41 +0200

    Upgrade setup

--- tests/index.test.tsx
+++ tests/index.test.tsx
@@ -1,4 +1,5 @@
-import { InterpolatedMaterialColors } from '../src';
+import { expect, test } from 'vitest';
+import { InterpolatedMaterialColors } from '../src/mod';
 
 test('colors at 500 match raw colors', () => {
   expect(InterpolatedMaterialColors.red(500)).toEqual('#F44336');
